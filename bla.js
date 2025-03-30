8510769f5901ed204d8d29b16b355c692aa52c22
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2025-03-30 00:19:31 -0300

    ..

--- bla.js
+++ bla.js
@@ -5,3 +5,6 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
   document.querySelector('.close-btn').addEventListener('click', function() {
     document.getElementById('mobile-overlay').classList.remove('active');
   });
+  setTimeout(function() {
+    document.getElementById('logo-animation').style.display = 'none';
+  }, 4000); // 4 segundos
