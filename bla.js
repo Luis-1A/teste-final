853e28b009d6af36aae3fe13075bd095bb3aef92
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2025-03-29 21:51:26 -0300

    ...

--- bla.js
+++ bla.js
@@ -1,29 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const divTest = document.getElementById("calabresa");
-
-    if (divTest) {
-        divTest.style.border = "2px solid red"; // Adiciona borda vermelha para testar
-        divTest.style.padding = "10px";
-    } else {
-        console.log("A div 'calabresa' não foi encontrada.");
-    }
-});
 
     const toggleBtn = document.getElementById('toggle-nav');
     const overlay = document.getElementById('overlay');
     const closeBtn = document.getElementById('close-nav');
 
-    // Abre o overlay ao clicar no botão "Menu"
+    // Abre o overlay
     toggleBtn.addEventListener('click', () => {
         overlay.classList.add('active');
     });
 
-    // Fecha o overlay ao clicar no "X"
+    // Fecha o overlay
     closeBtn.addEventListener('click', () => {
         overlay.classList.remove('active');
     });
 
-    // Fecha o overlay ao clicar fora da navegação
+    // Fecha ao clicar fora
     overlay.addEventListener('click', (e) => {
         if (e.target === overlay) {
             overlay.classList.remove('active');
