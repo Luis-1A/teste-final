5ce5ff32a687be56761ab1e1468c9fe943dd0b65
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2025-03-30 00:46:19 -0300

    kc

--- bla.js
+++ bla.js
@@ -5,4 +5,15 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
   document.querySelector('.close-btn').addEventListener('click', function() {
     document.getElementById('mobile-overlay').classList.remove('active');
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.logo-img');
+    const container = document.querySelector('.logo-container');
+    const containerWidth = container.offsetWidth;
+    const containerHeight = container.offsetHeight;
+    const logoWidth = logo.offsetWidth;
+    const logoHeight = logo.offsetHeight;
   
+    // Posiciona a logo no ponto (0,0), que é o centro do quadrado
+    logo.style.left = `${containerWidth / 2 - logoWidth / 2}px`;
+    logo.style.top = `${containerHeight / 2 - logoHeight / 2}px`;
+  });
