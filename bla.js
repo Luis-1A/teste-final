0d568959b7e356bee104901d7b63a751abbcfd66
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2025-03-29 22:17:29 -0300

    ..

--- bla.js
+++ bla.js
@@ -1,17 +1,6 @@
 const menuToggle = document.getElementById('menu-toggle');
 const mobileOverlay = document.getElementById('mobile-overlay');
-const closeOverlay = document.getElementById('close-overlay');
 
 menuToggle.addEventListener('click', () => {
-    mobileOverlay.classList.add('active');
-});
-
-closeOverlay.addEventListener('click', () => {
-    mobileOverlay.classList.remove('active');
-});
-
-mobileOverlay.addEventListener('click', (e) => {
-    if (e.target === mobileOverlay) {
-        mobileOverlay.classList.remove('active');
-    }
+    mobileOverlay.classList.toggle('active');
 });
