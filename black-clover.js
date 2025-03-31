2d2f9a9eae5794825893b66b1683ee0f2672f24d
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2025-03-30 23:41:28 -0300

    dd

--- black-clover.js
+++ black-clover.js
@@ -194,3 +194,45 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Verifique o console para mais detalhes durante a execução.');
     console.log('Fim do script.');
   });
+
+
+
+  document.querySelectorAll('.btn-buy').forEach(button => {
+    button.addEventListener('click', function (event) {
+        event.preventDefault(); // Evita comportamento padrão do botão
+        let product = this.closest('.product-item');
+        let title = product.querySelector('.title').innerText.trim();
+        let priceElement = product.querySelector('ins');
+        let price = priceElement ? priceElement.innerText.trim() : 'Preço não disponível';
+        
+        let message = `Olá, estou interessado no produto: ${title}, que está no valor de ${price}. Poderia me dar mais informações?`;
+        let phoneNumber = '6191947884';
+        let whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        
+        window.location.href = whatsappUrl;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+let closeCounter = 0;
+document.querySelectorAll('.btn-buy').forEach(button => {
+    button.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+        closeCounter++;
+        if (closeCounter === 3) {
+            console.log('%cPensando...', 'color: blue; font-size: 20px;');
+            setTimeout(() => {
+                console.log('%cCódigo JavaScript ativado!', 'color: green; font-size: 20px;');
+            }, 1000);
+            closeCounter = 0;
+        }
+    });
+});
